5a0c1bf68c4e0fdfd814a0bd7b1bf22cc5fb7dd9
Samuel seneni <samuelmunialo2005@gmail.com>
2023-12-04 06:35:19 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines an object attribute and object function."""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods"""
+    return (dir(obj))
